04a750a1e32d387b4866aaa800f244a671561666
chachaxw <867571123@qq.com>
2016-10-13 17:25:23 +0800

    添加webview component

--- toilet/ios_views/ToiletPage.js
+++ toilet/ios_views/ToiletPage.js
@@ -8,12 +8,15 @@ import {
   View,
 } from 'react-native';
 const NavigationBar = Navigator.NavigationBar;
+const nearByURL = 'http://123.57.39.116:3000/html/nearby.html';
+// const nearByURL = './../html/location.html';
 
 class ToiletPage extends Component {
   render() {
     return (
       <View>
         <Text style={style.alignCenter}>卫生间</Text>
+        <WebView source={{uri: nearByURL}}></WebView>
       </View>
     );
   };
