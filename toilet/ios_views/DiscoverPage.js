499332c88df38bff91374486e570266931988a7a
chachaxw <867571123@qq.com>
2016-10-28 23:54:04 +0800

    添加发现页面

--- toilet/ios_views/DiscoverPage.js
+++ toilet/ios_views/DiscoverPage.js
@@ -17,6 +17,102 @@ import Category from './read/Category';
 import Topic from './read/Topic';
 import Search from './read/Search';
 
+class ReadView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShow: false,
+      recommendTopic: null,
+      hotTopic: null,
+      category: null,
+      other: null,
+      refreshing: false
+    };
+  }
+
+  componentDidMount(){
+    this._fetchData();
+  }
+
+  _fetchData(callback){
+    var self = this;
+    Util.get('http://123.57.39.116:3000/data/read?type=config', function(data){
+      if(data.status){
+        let obj = data.data;
+        self.setState({
+          isShow: true,
+          recommendTopic: obj.recommendTopic,
+          hotTopic: obj.hotTopic,
+          category: obj.category,
+          other: obj.other,
+          refreshing: false
+        });
+      }else{
+        alert('服务异常,正在紧急修复,请耐心等待');
+      }
+    }, function(err){
+      alert(err);
+      alert('服务异常,正在紧急修复,请耐心等待2');
+    });
+  }
+
+  _onRefresh(){
+    var self = this;
+    this.setState({refreshing: true});
+    this._fetchData();
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Search navigator={this.props.navigator}/>
+        {
+          this.state.isShow ?
+            (<ScrollView
+              refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh.bind(this)}
+              />
+              }
+              style={[styles.container, {paddingTop:20}]}>
+              <Topic data={this.state.recommendTopic} navigator={this.props.navigator} type="manager"/>
+              <HrLine/>
+              <Recommend title="热门推荐" data={this.state.hotTopic} navigator={this.props.navigator} type="it"/>
+              <HrLine/>
+              <Category data={this.state.category} navigator={this.props.navigator}/>
+              <HrLine/>
+              <Recommend title="清新一刻" data={this.state.other} navigator={this.props.navigator} type="sanwen"/>
+              <Space/>
+            </ScrollView>)
+            :
+            (<ActivityIndicator
+              animating={true}
+              style={[{height: 80}]}
+              size="large"
+              />)
+        }
+      </View>
+    )
+  };
+}
+
+class HrLine extends Component{
+  render(){
+    return (
+      <View style={styles.hr}></View>
+    );
+  }
+}
+
+class Space extends Component{
+  render(){
+    return (
+      <View style={styles.space}></View>
+    );
+  }
+}
+
 class DiscoverPage extends Component {
   render() {
     return (
